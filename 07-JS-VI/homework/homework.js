eca1569e23b9fdccae72fc4c48d83faa0079d653
Jose Rivera <jose.riverav@udea.edu.co>
2022-01-21 11:05:39 -0500

    Se completo hemework del modulo VI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,22 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var x = String.fromCharCode(nombre.charCodeAt(0)-32);
+  nombre=nombre.replace(nombre[0],x);
+  return nombre;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
-  //Tu código:
+  // Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +27,18 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var sum=0;
+  for(let n in numeros){
+    sum+=numeros[n];
+  }
+  cb(sum);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(cb);
 }
 
 function map(array, cb) {
@@ -35,13 +46,25 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  return array.map(cb);
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  function iniciaA(str){
+    if(str[0]=="a"){
+      return str;
+    }
+    else{
+      return "";
+    }
+  }
+  var arr=array.map(iniciaA).filter(word => word.length>0);
+  return arr;
 }
+console.log(filter(['abajo', 'pera', 'escalera', 'alerta', 'indice', 'azteca', 'arbol', 'buzo']));
 
 // No modificar nada debajo de esta línea
 // --------------------------------
